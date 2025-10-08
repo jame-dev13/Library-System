6834767c770c9f118a0d40c7de8d3d701e5e0694
jame-dev13 <jam.dev013@gmail.com>
2025-10-08 12:15:34 -0600

    feat(Utils): Methods for validations and test added.

--- src/main/java/jame/dev/utils/ValidatorUtil.java
+++ src/main/java/jame/dev/utils/ValidatorUtil.java
@@ -2,14 +2,52 @@ package jame.dev.utils;
 
 public final class ValidatorUtil {
 
-   public static boolean isValidString(String... s){
-      boolean matches = false;
-      boolean isBlank = false;
+   public static boolean isValidString(String... s) {
+      if(s == null || s.length == 0) return false;
       for (String string : s) {
-         matches = string.matches("^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ@., ]+$");
-         isBlank = string.isBlank();
+         if(string == null || string.isBlank() || !string.matches("^[a-zA-záéíóúÁÉÍÓÚñÑ][a-zA-Z0-9áéíóúÁÉÍÓÚñÑ@'. ]+$")){
+            return false;
+         }
+      }
+      return true;
+   }
+
+   public static boolean isEmailValid(String s) {
+      return s.matches("^[a-zA-Z][a-zA-Z0-9.-_]+@+[a-zA-Z0-9.-]+\\.[a-zA-Z0-9]{2,}$");
+   }
+
+   public static boolean validPassword(String s) {
+      final int MIN_LENGTH = 11;
+      final int MIN_UPPER = 3;
+      final int MIN_LOWER = 3;
+      final int MIN_DIGIT = 2;
+      final int MIN_SPEC = 2;
+
+      if (s == null || s.isBlank() || s.length() < MIN_LENGTH) {
+         return false;
       }
 
-      return !isBlank && matches;
+      int upper = 0, lower = 0, digit = 0, spec = 0;
+
+      for (char c : s.toCharArray()) {
+         if (Character.isSpaceChar(c))
+            return false;
+         else if (Character.isUpperCase(c)) {
+            upper++;
+         } else if (Character.isLowerCase(c)) {
+            lower++;
+         } else if (Character.isDigit(c)) {
+            digit++;
+         } else {
+            spec++;
+         }
+
+         if (upper >= MIN_UPPER && lower >= MIN_LOWER && digit >= MIN_DIGIT && spec >= MIN_SPEC) {
+            return true;
+         }
+      }
+
+      return upper >= MIN_UPPER && lower >= MIN_LOWER && digit >= MIN_DIGIT && spec >= MIN_SPEC;
    }
+
 }
